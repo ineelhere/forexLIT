2dddbaf84922b9f47ef9399dbd545bfcaa86e344
ineelhere <you@example.com>
2022-10-11 23:33:37 +0530

    [ADD] gifs and info

--- pages/3_👨🏼‍💻_About.py
+++ pages/3_👨🏼‍💻_About.py
@@ -2,7 +2,8 @@ import streamlit as st
 from footer import *
 
 st.balloons()
-st.header("ForexLIT")
+st.header("ABOUT ForexLIT")
+st.sidebar.success("The purpose and development setup of ForexLIT")
 
 st.markdown('''
 * This is a light-weight web application that fetches realtime data for several Foreign and Crypto currency exchange ratesand allows user to find the realtime status of the same and perform currency conversions. 
@@ -15,4 +16,7 @@ st.markdown('''
 * For any queries and feedback please reach out to [Indraneel](https://www.linkedin.com/in/indraneelchakraborty/).
 ''')
 
+st.sidebar.markdown('''
+<div style="padding-top:100.000%;position:relative;"><iframe src="https://gifer.com/embed/bf6" width="100%" height="100%" style='position:absolute;top:0;left:0;' frameBorder="0" allowFullScreen></iframe></div><p><a href="https://gifer.com">via GIFER</a></p>
+''', unsafe_allow_html=True)
 footer()
